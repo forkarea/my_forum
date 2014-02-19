4a2b121a0f7070c1d975505ffe970b64e395245b
Mateusz Jończyk <mat.jonczyk@o2.pl>
2014-02-19 13:52:04 +0100

    Corrected the display style

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8">
 </head>
 <body>
+<table style="width: 70%" >
 <?php
 try {
         //phpinfo();
@@ -14,9 +15,9 @@ try {
         $stmt = $dbh->prepare('SELECT text from posts');
         if ($stmt->execute()) { 
                 while($row = $stmt->fetch()) {
-                        echo '<p>';
+                        echo '<tr><td style="border:1px solid black; padding:10px">';
                                 echo $row[0];
-                        echo '</p>';
+                        echo '</td></tr>';
                 }
         }
 } catch (PDOException $e) {
@@ -24,6 +25,7 @@ try {
 }
 
 ?>
+</table>
 <form action="index_upload.php" method="post">
 <p><textarea rows="5" cols="20" name="text">New post</textarea></p>
 <p><input type="submit"></p>
