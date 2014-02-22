19846d8f5d2b5a2c46f419bcdd12cf57272ee385
Mateusz Jończyk <mat.jonczyk@o2.pl>
2014-02-22 16:09:44 +0100

    Sending cookies as HTTP_ONLY

--- classes/User.php
+++ classes/User.php
@@ -40,12 +40,11 @@ if (PHP_VERSION_ID < 50500) {
                         } catch (PDOException $ex) {
                                 return "Cannot connect to database";
                         }                    
-                        $r1 = setcookie("login", $this->login, time() + 30*24*3600/*, 'localhost', '/', FALSE, TRUE*/);
+                        $r1 = setcookie("login", $this->login, time() + 30*24*3600, '/', NULL, FALSE, TRUE);
                         if (!$r1) {
                                 return "Cannot set login cookie.";
                         }
-                        $r2 = setcookie("login_cookie", $login_token, time() + 30*24*3600/*, 'localhost', '/', FALSE, TRUE*/);
-                        
+                        $r2 = setcookie("login_cookie", $login_token, time() + 30*24*3600, '/', NULL, FALSE, TRUE);
                         if (!$r2) {
                                 return "Cannot set login_cookie cookie.";
                         }
